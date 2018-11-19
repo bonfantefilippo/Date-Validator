6daa6ca860ec55ea5adfa670df25c957c00ec9d0
Filippo Bonfante <bonfantefilippo@gmail.com>
2018-11-19 22:02:51 +0100

    Aggiunta funzione data precedente a quella inserita; cleaned code;

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 
 int main(int argc, char *argv[]) {
 	int day, month, year;
+	int prevDay, prevMonth, prevYear;
+	
+	prevDay = day;
+	prevMonth = month;
+	prevYear = year;
 	
 	printf("Ciao! Sono un validatore di date!");
 	printf("\nInserisci un giorno valido: ");
@@ -20,6 +25,10 @@ int main(int argc, char *argv[]) {
 	printf("Hai inserito l'anno %d \n", year);
 	printf("\n", year);
 	
+	prevDay = day;
+	prevMonth = month;
+	prevYear = year;
+	
 	
 	if(checkDate(&day, &month, &year)==0) {
 		printf("\nLa data %d/%d/%d non e' una data valida.", day, month, year);
@@ -28,7 +37,10 @@ int main(int argc, char *argv[]) {
 		printf("\nLa data %d/%d/%d e' una data valida.", day, month, year);
 	}
 	
+	prevDate(&prevDay, &prevMonth, &prevYear);
 	
+	printf("\nData precedente a quella inserita: %d/%d/%d", prevDay, prevMonth, prevYear);
+
 	return 0;
 }
 
@@ -101,7 +113,7 @@ int initialCheck(int *day, int *month, int *year){
 
 /*
 	0 non bisestile
-	1 è bisestile
+	1 bisestile
 */
 int checkYear(int *year) {
 	if(*year%4==0){
@@ -127,10 +139,70 @@ int checkYear(int *year) {
 
 
 
+/*
+	FUNZIONE CHE TORNA DATA PRECEDENTE AD UNA VALIDA INSERITA:
+	1. Se il mese inserito è 1 (gennaio) e il giorno è 1, 
+		allora: giorno 31, mese 12, anno = anno -1   //FUNZIONA
+		
+	2. Se il mese inserito è 3 (marzo) e il giorno è 1, allora:
+		2.1 verifico se l'anno è bisestile, se bisestile giorno=29 mese=2 anno=stesso anno
+		2.2 se non è bisestile allora giorno = 28 mese = 2 //FUNZIONA
+	
+	3. Considerare altre opzioni
+		
+	
+*/
+void prevDate(int *day, int *month, int *year){
+	if(checkDate(day, month, year)==1){
+		
+		// verifico primo giorno dell'anno
+		if(*day==1 && *month==1) {
+			*day=31; 
+			*month=12;
+			*year = *year -1;
+		} //verifico febbraio
+		else if(*day==1 && *month==3){
+			if(checkYear(year) == 0){
+				*day = 28;
+				*month = 2;
+			} else {
+				*day = 29;
+				*month = 2;
+			}
+		} else if((*month==5 || *month==7 || *month==8 || *month==10 || *month==12) && *day==1) {
+			*day = 30;	
+			*month = *month - 1;
+			
+		} else if((*month==2 || *month==4 || *month==6 || *month==9 || *month==11) && *day==1) {
+			*day = 31;	
+			*month = *month - 1;
+		} else if(*month>0 && *month<=12 && *day!=1){
+			*day= *day -1;
+		}
+		
+	} else {
+    	printf("\nLa data %d/%d/%d non e' una data valida.", *day, *month, *year);
+	}
+	
+}
+
+
+/*
+	FUNZIONE CHE CALCOLA DISTANZA TRA DUE DATE
+	1. La prima è quella più recente
+	2. Decremento la data finche' la non raggiungo la data precedente
+*/
+void gapDate(int* day, int *month, int* year){
+	
+}
+
+
+
+
 
 /*
 	gennaio, di 31 giorni
-	febbraio, di 28 giorni (29 se l'anno è bisestile)
+	febbraio, di 28 giorni (29 se l'anno ï¿½ bisestile)
 	marzo, di 31 giorni
 	aprile, di 30 giorni
 	maggio, di 31 giorni
